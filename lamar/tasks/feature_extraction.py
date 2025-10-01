a4fd747dbc2c71f22e5e45d210ccc31b574a5e1e
longluv1605 <longtrong53@gmail.com>
2025-10-01 10:15:03 +0700

    mod:  superpoint  resize  640

--- lamar/tasks/feature_extraction.py
+++ lamar/tasks/feature_extraction.py
@@ -46,7 +46,7 @@ class FeatureExtraction:
                 },
                 'preprocessing': {
                     'grayscale': True,
-                    'resize_max': 2000,
+                    'resize_max': 640,
                 },
             },
         },
